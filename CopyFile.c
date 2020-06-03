5cbb94bcfbc4479c24fa7712af224431493acd97
GLozada99 <57233171+GLozada99@users.noreply.github.com>
2020-06-03 13:48:02 -0400

    Update CopyFile.c

--- CopyFile.c
+++ CopyFile.c
@@ -10,21 +10,21 @@ int main(int narg, char* argv[]){
     if(narg>=3){
         FILE *start, *end;
         size_t size_read = 1, size_write = 1;
-        char buffer[65536], overwr[100];
-        char source[100], dest[100];
+        char *buffer, *overwr;
+        char *source, *dest;
         int arguments = 3;
-        char choise='U';
-        int overwrite = 0;
+        char choise='N';
+        int overwrite = 1;
 
-        strcpy(overwr,argv[1]);
+        overwr = argv[1];
         
-        if(strcmp(overwr,"-i")==0){
+        if(strcmp(overwr,"-i")==0){ 
             arguments = 4;
-            strcpy(source,argv[2]);
-            strcpy(dest,argv[3]);
+            source = argv[2];
+            dest = argv[3];
         }else{
-            strcpy(source,argv[1]);
-            strcpy(dest,argv[2]);
+            source = argv[1];
+            dest = argv[2];
         }
         
         if(narg==arguments){
@@ -41,50 +41,39 @@ int main(int narg, char* argv[]){
                     fflush(stdin);
                     scanf(" %c", &choise);
                     choise = toupper(choise);
-                    
                     }
-                
+                    if(choise=='N'){
+                        overwrite = 0;
+                        printf("Copy aborted\n");
+                        return 0;
+                    }
                 }
             }else{
                 if(access(dest,F_OK)==0){
+                    overwrite = 0;
                     printf("Destination File already exists. Copy aborted\n");
                     return 0;
-                }else{
-                    overwrite = 1;
-                }
-                
+                } 
             }
-            if(choise=='N'){
-                printf("Copy aborted\n");
-                return 0;
-            }else{
-                overwrite = 1;
-            }
-            
             if(overwrite==1){
-                
                 start = fopen(source,"rb");
                 end = fopen(dest,"wb");
-                if(start!=NULL && end!=NULL){
-                    
-                    
+
+                if(start!=NULL){
                     while((size_write == size_read) && size_read){
                         size_read = fread(buffer,1,sizeof(buffer),start);
-                        
-                        
                         if(size_read!=0){
                             size_write = fwrite(buffer,1,size_read,end);
                             
                         }else{
                             size_write = 0;
                         }
-                        
-                        
                     }
                     printf("File copy was succesful\n\n");
                 }else{
                     printf("File %s doesn't exist\n\n",source);
                 }
+
             fclose(start);
             fclose(end);
             }
